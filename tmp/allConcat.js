09c356c2ca0dc045c4354305b0c185b6f83c4261
Diane Douglas <diane.douglas1@gmail.com>
2016-02-15 17:00:45 -0800

    Add error checking.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -31,7 +31,11 @@ $(document).ready(function(){
     var city = $('#location').val();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      if (response.cod !== 200) {
+        $('.showWeather').text(response.message);
+      } else {
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      }
     });
   });
 });
