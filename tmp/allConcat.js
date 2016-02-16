02828032b36622cf18151962cbabfaf504c1aa2d
Diane Douglas <diane.douglas1@gmail.com>
2016-02-15 16:50:59 -0800

    Add API call.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -24,12 +24,15 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = "YOUR-API-KEY-GOES-HERE";
+var apiKey = "e572e1a4c7b5d74308c11f537d1682e0";
 
 $(document).ready(function(){
   $('#weatherLocation').click(function(){
     var city = $('#location').val();
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
   });
 });
