94e4e03f89ebda82e405f98b9ca69705a174d493
dianedouglas <diane.douglas1@gmail.com>
2016-02-16 02:05:50 -0800

    Add temperature-interface.js file.

--- build/js/app.js
+++ build/js/app.js
@@ -38,6 +38,22 @@ $(document).ready(function(){
   });
 });
 
+var apiKey = "e572e1a4c7b5d74308c11f537d1682e0";
+
+$(document).ready(function(){
+  $('#getTemperature').click(function(){
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      if (response.cod !== 200) {
+        $('.showWeather').text(response.message);
+      } else {
+        $('.showWeather').text("The temperature in " + city + " is " + response.main.temp + "degrees.");
+      }
+    });
+  });
+});
+
 $(document).ready(function(){
   $('#time').text(moment());
 });
