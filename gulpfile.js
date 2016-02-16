43f6149e6f975ed19cf0a088ba8b8ba86e6f1c45
Diane Douglas <diane.douglas1@gmail.com>
2016-02-15 16:09:00 -0800

    Add watch tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -81,4 +81,14 @@ gulp.task('serve', function() {
       index: "index.html"
     }
   });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['minifyScripts', 'jshint'], function(){
+  browserSync.reload();
+});
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
