e3e1fdcfd77246c2f1b17fb05a16206ea5466d50
Staff <staff@staff-7.local>
2016-03-10 12:01:01 -0800

    Add promise.

--- build/js/app.js
+++ build/js/app.js
@@ -45,20 +45,22 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = require('./../config.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $('#weatherLocation').click(function(){
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       if (response.cod !== 200) {
         $('.showWeather').text(response.message);
       } else {
         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
       }
+    }).fail(function(error){
+      $('.showWeather').text(error.message);
     });
   });
 });
 
-},{"./../config.env":1,"./../js/ping-pong.js":2}]},{},[3]);
+},{"./../.env":1,"./../js/ping-pong.js":2}]},{},[3]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -24,18 +24,20 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = require('./../config.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $('#weatherLocation').click(function(){
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       if (response.cod !== 200) {
         $('.showWeather').text(response.message);
       } else {
         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
       }
+    }).fail(function(error){
+      $('.showWeather').text(error.message);
     });
   });
 });
