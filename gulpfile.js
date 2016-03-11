7cd19c7f903ea41d8fed5c7604e4b26fc3b6b976
Staff <staff@staff-3.local>
2016-03-10 16:46:15 -0800

    Add jshint task and fix errors.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var buildProduction = utilities.env.production;
+var jshint = require('gulp-jshint');
 
 gulp.task('concatInterface', function(){
   return gulp.src(['./js/*-interface.js'])
@@ -37,3 +38,9 @@ gulp.task('build', ['clean'], function(){
     gulp.start('jsBrowserify');
   }
 });
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+})
